25e6bac71e25cd539542d940224fa7455efa4007
pratham1233-cmyk <prathamankur123@gmail.com>
2026-06-27 10:01:06 +0530

    Update string_compression.c

--- string_compression.c
+++ string_compression.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    char s[200], result[400];
+int main()
+{
+    char str[100], result[100];
+    int i = 0, j = 0, count;
+
     printf("Enter string: ");
-    scanf("%s", s);
-    int i = 0, j = 0;
-    while (s[i] != '\0') {
-        char ch = s[i];
-        int count = 0;
-        while (s[i] == ch && s[i] != '\0') { count++; i++; }
-        result[j++] = ch;
-        if (count > 1) {
-            int digits[10], d = 0;
-            while (count > 0) { digits[d++] = count % 10; count /= 10; }
-            for (int k = d - 1; k >= 0; k--) result[j++] = '0' + digits[k];
+    scanf("%s", str);
+
+    while(str[i] != '\0')
+    {
+        count = 1;
+
+        while(str[i] == str[i + 1])
+        {
+            count++;
+            i++;
         }
+
+        result[j] = str[i];
+        j++;
+
+        if(count > 1)
+        {
+            result[j] = count + '0';
+            j++;
+        }
+
+        i++;
     }
+
     result[j] = '\0';
-    printf("Compressed: %s\n", result);
+
+    printf("Compressed String: %s", result);
+
     return 0;
 }
