8383ce6bca4241cc24b49a6850188247766b5cc6
pratham1233-cmyk <prathamankur123@gmail.com>
2026-06-27 09:57:31 +0530

    Update moores_voting.c

--- moores_voting.c
+++ moores_voting.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
 
-int main() {
-    int n;
+int main()
+{
+    int n, i;
+    
     printf("Enter size: ");
     scanf("%d", &n);
+
     int arr[n];
+
     printf("Enter elements: ");
-    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
-    int candidate = arr[0], count = 1;
-    for (int i = 1; i < n; i++) {
-        if (arr[i] == candidate) count++;
-        else { count--; if (count == 0) { candidate = arr[i]; count = 1; } }
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
     }
+
+    int candidate = arr[0];
+    int count = 1;
+
+    for(i = 1; i < n; i++)
+    {
+        if(arr[i] == candidate)
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+
+            if(count == 0)
+            {
+                candidate = arr[i];
+                count = 1;
+            }
+        }
+    }
+
     count = 0;
-    for (int i = 0; i < n; i++) if (arr[i] == candidate) count++;
-    if (count > n / 2) printf("Majority element: %d\n", candidate);
-    else printf("No majority element\n");
+
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == candidate)
+        {
+            count++;
+        }
+    }
+
+    if(count > n / 2)
+    {
+        printf("Majority Element = %d", candidate);
+    }
+    else
+    {
+        printf("No Majority Element");
+    }
+
     return 0;
 }
